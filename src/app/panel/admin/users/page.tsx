683ffca0374333ab7587ac000874e3dc81b2a4c3
hidirektor <hidirektor@gmail.com>
2026-05-06 17:29:29 +0300

    UI Update

--- src/app/panel/admin/users/page.tsx
+++ src/app/panel/admin/users/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, {useEffect, useState} from "react";
-import {apiUrl} from "../../../../../lib/api";
+import {apiUrl} from "../../../../lib/api";
 
 export default function AdminUsersPage() {
   const [users, setUsers] = useState<any[]>([]);
