4f8a2e3a967691ad240f18af0eb25115500f2d23
hidirektor <hidirektor@gmail.com>
2026-05-06 16:05:32 +0300

    UI Update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -136,7 +136,6 @@ const Header = () => {
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
                       <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
                     </svg>
-                    <span className="hidden lg:inline">{t("header.favorites")}</span>
                   </Link>
 
                   {/* Cart Label & Icon */}
@@ -147,7 +146,6 @@ const Header = () => {
                     <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="1.5">
                       <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
                     </svg>
-                    <span className="hidden lg:inline text-[13px] font-bold uppercase tracking-[0.1em]">{t("header.cart_label")}</span>
                     <span className="absolute -top-1.5 -right-2 bg-primary text-white text-[9px] font-black h-4 w-4 rounded-full flex items-center justify-center border-2 border-background">
                       0
                     </span>
