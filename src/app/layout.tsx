446b8095c5b908a124aa89f1c9087452494464c9
mustafa <mustafamavedda@gmail.com>
2026-05-15 12:57:29 +0300

    Hata Düzeltmesi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,17 @@ export const metadata = {
   },
 };
 
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: '#f9fafb' },
+    { media: '(prefers-color-scheme: dark)', color: '#000000' }
+  ],
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
